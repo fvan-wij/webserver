b4ac646ce948b9553409dc54a82d4849c2fd4ab0
joppe <jboeve009@gmail.com>
2024-09-04 23:23:32 +0200

    Add leading zero to timestamp

--- src/logging/Logger.cpp
+++ src/logging/Logger.cpp
@@ -1,8 +1,8 @@
 #include "Logger.hpp"
 #include "meta.hpp"
 #include "ansi.hpp"
-#include <chrono>
 #include <ctime>
+#include <iomanip>
 #include <iostream>
 #include <string>
 
@@ -58,12 +58,17 @@ void Logger::operator()(std::string const& message, char const* function, char c
 		COLOR_WHITE,
 	};
 
+    const time_t t = time(0);  
+    const struct tm ct = *localtime(&t);
 
-    time_t t = time(0);  
-    struct tm ct = *localtime(&t);
 
 
-	std::cout << COLORS[int(_level)] << "[" << ct.tm_hour << ":" << ct.tm_min << ":" << ct.tm_sec << "]" << PREFIX[int(_level)] << message;
+	std::stringstream time;
+	time	<< "[" << std::setfill('0') << std::setw(2) << ct.tm_hour
+			<< ":" << std::setfill('0') << std::setw(2) << ct.tm_min
+			<< ":" << std::setfill('0') << std::setw(2) << ct.tm_sec << "]";
+
+	std::cout << COLORS[int(_level)] << time.str() << PREFIX[int(_level)] << message;
 	if (_level == Level::DEBUG)
 		std::cout << "\t @ " << file << ":" << line;
 
