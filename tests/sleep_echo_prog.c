f439ef5d0eac4d495aeeb5b34492d7355194800b
joppe <jboeve009@gmail.com>
2025-01-04 22:54:06 +0100

    Can run multiple CGI's

--- tests/sleep_echo_prog.c
+++ tests/sleep_echo_prog.c
@@ -13,24 +13,34 @@ int main(int argc, char **argv)
 		return 1;
 	}
 	(void) argc;
+
 	const int32_t sleep_time = atoi(argv[1]);
 	printf("sleeping for [%i]\n", sleep_time);
-	sleep(sleep_time);
-	printf("done sleeping\n");
+
+	struct timeval start;
+	gettimeofday(&start, NULL);
 
 	time_t rawtime;
 	struct tm * timeinfo;
+	time ( &rawtime );
+	timeinfo = localtime ( &rawtime );
+	printf ("start time and date: %s", asctime (timeinfo) );
+
+	sleep(sleep_time);
+	printf("done sleeping\n");
+
 
 	time ( &rawtime );
 	timeinfo = localtime ( &rawtime );
-	printf ( "Current local time and date: %s", asctime (timeinfo) );
+	printf ("end time and date: %s", asctime (timeinfo) );
 
 	struct timeval stop;
 	//do stuff
 	gettimeofday(&stop, NULL);
-	printf("took %lu us\n", (stop.tv_sec) * 1000000 + stop.tv_usec);
-	// system("python3 -m http.server 9001");
-	execl("/home/joppe/.pyenv/shims/python3", "python3", "-m", "http.server", "8997", (char *) NULL);
+    int elapsedTime = (stop.tv_sec - start.tv_sec) * 1000.0;      // sec to ms
 
+	// printf("took %lu us\n", (stop.tv_sec) * 1000000 + stop.tv_usec);
+	printf("took %d ms\n", elapsedTime);
+	// execl("/home/joppe/.pyenv/shims/python3", "python3", "-m", "http.server", "8997", (char *) NULL);
 	return 0;
 }
