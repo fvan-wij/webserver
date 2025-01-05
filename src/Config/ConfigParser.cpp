f11597181997e24adc2031062d55349599e79d30
Duco Ritsema van Eck <ducorits@zonderstreepje.nl>
2025-01-05 01:04:39 +0100

    Fixed typo in test2.conf, and removed 100 lines from config parser

--- src/Config/ConfigParser.cpp
+++ src/Config/ConfigParser.cpp
@@ -11,166 +11,110 @@
 static std::vector<std::string> tokenize_string(std::string string, std::string delimiters)
 {
 		std::vector<std::string>	tokens;
-		size_t						pos = 0;
+		size_t						start = 0;
+		size_t						end = 0;
 
-		while (pos != std::string::npos)
+		start = string.find_first_not_of(delimiters, end);
+		while (start != std::string::npos)
 		{
-			pos = string.find_first_not_of(delimiters);
-			string.erase(0, pos);
-			pos = string.find_first_of(delimiters);
-			std::string token = string.substr(0, pos);
-			if (token.empty())
-				break;
-			tokens.push_back(token);
-			string.erase(0, pos);
+			end = string.find_first_of(delimiters, start);
+			tokens.push_back(string.substr(start, end - start));
+			start = string.find_first_not_of(delimiters, end);
 		}
-		if (!string.empty())
-			tokens.push_back(string);
 		return tokens;
 }
 
-std::string			 parse_root(std::vector<std::string> tokens, unsigned long &i)
+std::string	parse_string(std::vector<std::string> tokens, unsigned long &i)
 {
-	std::string root;
 	i++;
-	root = tokens[i].substr(0, tokens[i].size() - 1);
+	std::string str = tokens[i].substr(0, tokens[i].size() - 1);
 	i++;
-	return (root);
+	return (str);
+}
+
+std::vector<std::string> parse_list(std::vector<std::string> tokens, unsigned long &i)
+{
+	std::vector<std::string>	list;
+	i++;
+	while (tokens[i].find(";") == std::string::npos)
+	{
+		list.push_back(tokens[i]);
+		i++;
+	}
+	list.push_back(tokens[i].substr(0, tokens[i].size() - 1));
+	i++;
+	return list;
 }
 
 std::pair<std::string, int> parse_listen(std::vector<std::string> tokens, unsigned long &i)
 {
-	std::pair<std::string, int> listen;
 	i++;
-	listen.first = tokens[i].substr(0, tokens[i].find(":"));
-	listen.second = std::stoi(tokens[i].substr(tokens[i].find(":") + 1, tokens[i].size()));
+	std::string address = tokens[i].substr(0, tokens[i].find(":"));
+	int port = std::stoi(tokens[i].substr(tokens[i].find(":") + 1, tokens[i].size()));
 	i++;
-	return (listen);
-}
-
-std::string parse_index(std::vector<std::string> tokens, unsigned long &i)
-{
-	std::string index;
-	i++;
-	index = tokens[i].substr(0, tokens[i].size() - 1);
-	i++;
-	return (index);
-}
-
-std::vector<std::string> parse_server_name(std::vector<std::string> tokens, unsigned long &i)
-{
-	std::vector<std::string>	server_name;
-	i++;
-	while (tokens[i].find(";") == std::string::npos)
-	{
-		server_name.push_back(tokens[i]);
-		i++;
-	}
-	server_name.push_back(tokens[i].substr(0, tokens[i].size() - 1));
-	i++;
-	return (server_name);
-}
-
-std::vector<std::string>	parse_allow_methods(std::vector<std::string> tokens, unsigned long &i)
-{
-	std::vector<std::string>	methods;
-	i++;
-	while (tokens[i].find(";") == std::string::npos)
-	{
-		methods.push_back(tokens[i]);
-		i++;
-	}
-	methods.push_back(tokens[i].substr(0, tokens[i].size() - 1));
-	i++;
-	return (methods);
+	return {address, port};
 }
 
 std::pair<int, std::string> parse_redirection(std::vector<std::string>& tokens, unsigned long &i)
 {
-	std::pair<int, std::string> redirection;
 	i++;
-	redirection.first = std::stoi(tokens[i]);
+	int code = std::stoi(tokens[i]);
 	i++;
-	redirection.second = tokens[i].substr(0, tokens[i].size() - 1);
+	std::string url = tokens[i].substr(0, tokens[i].size() - 1);
 	i++;
-	return (redirection);
+	return {code, url};
 }
 
 bool parse_autoindex(std::vector<std::string> tokens, unsigned long &i)
 {
-	bool autoindex;
 	i++;
-	if (tokens[i] == "on;")
-	{
-		autoindex = true;
-	}
-	else
-		autoindex = false;
+	bool autoindex = tokens[i] == "on;" ? true : false;
 	i++;
 	return (autoindex);
 }
 
-std::pair<std::string, Location> parse_location(std::vector<std::string> tokens, unsigned long &i)
-{
-	Location	location;
-	location.autoindex = false;
-	std::pair<std::string, Location> entry;
-	i++;
-	location.path = tokens[i];
-	entry.first = location.path;
-	while (tokens[i] != "}")
-	{
-		if (tokens[i] == "root")
-		{
-			location.root = parse_root(tokens, i);
-			continue;
-		}
-		if (tokens[i] == "index")
-		{
-			location.index = parse_index(tokens, i);
-			continue;
-		}
-		if (tokens[i] == "allow_methods")
-		{
-			location.allowed_methods = parse_allow_methods(tokens, i);
-			continue;
-		}
-		if (tokens[i] == "return")
-		{
-			location.redirection = parse_redirection(tokens, i);
-			continue;
-		}
-		if (tokens[i] == "autoindex")
-		{
-			location.autoindex = parse_autoindex(tokens, i);
-			continue;
-		}
-		i++;
-	}
-	i++;
-	entry.second = location;
-	return (entry);
-}
-
 int	parse_client_max_body_size(std::vector<std::string> tokens, unsigned long &i)
 {
-	int client_max_body_size;
 	i++;
-	client_max_body_size = std::stoi(tokens[i]);
+	int client_max_body_size = std::stoi(tokens[i]);
 	i++;
 	return (client_max_body_size);
 }
 
 std::pair<int, std::string> parse_error_page(std::vector<std::string> tokens, unsigned long &i)
 {
-	std::pair<int, std::string> error_page;
 	i++;
-	error_page.first = std::stoi(tokens[i]);
+	int code = std::stoi(tokens[i]);
 	i++;
-	error_page.second = tokens[i].substr(0, tokens[i].size() - 1);
+	std::string path = tokens[i].substr(0, tokens[i].size() - 1);
 	i++;
-	return (error_page);
+	return {code, path};
 
+}
+
+std::pair<std::string, Location> parse_location(std::vector<std::string> tokens, unsigned long &i)
+{
+	Location	location;
+	location.autoindex = false;
+	i++;
+	location.path = tokens[i];
+	while (tokens[i] != "}")
+	{
+		if (tokens[i] == "root")
+			location.root = parse_string(tokens, i);
+		else if (tokens[i] == "index")
+			location.index = parse_string(tokens, i);
+		else if (tokens[i] == "allow_methods")
+			location.allowed_methods = parse_list(tokens, i);
+		else if (tokens[i] == "return")
+			location.redirection = parse_redirection(tokens, i);
+		else if (tokens[i] == "autoindex")
+			location.autoindex = parse_autoindex(tokens, i);
+		else
+			i++;
+	}
+	i++;
+	return {location.path, location};
 }
 
 Config	read_config(std::vector<std::string> tokens, unsigned long &i)
@@ -179,52 +123,25 @@ Config	read_config(std::vector<std::string> tokens, unsigned long &i)
 	while (i < tokens.size())
 	{
 		if (tokens[i] == "root")
-		{
-			server_config.root = parse_root(tokens, i);
-			continue ;
-		}
-		if (tokens[i] == "listen")
-		{
+			server_config.root = parse_string(tokens, i);
+		else if (tokens[i] == "listen")
 			server_config.listen.push_back(parse_listen(tokens, i));
-			continue;
-		}
-		if (tokens[i] == "index")
-		{
-			server_config.index = parse_index(tokens, i);
-			continue;
-		}
-		if (tokens[i] == "server_name")
-		{
-			server_config.server_name = parse_server_name(tokens, i);
-			continue;
-		}
-		if (tokens[i] == "location")
-		{
-			std::pair<std::string, Location>	location;
-			location = parse_location(tokens, i);
-			server_config.location.insert(location);
-			continue;
-		}
-		if (tokens[i] == "allow_methods")
-		{
-			server_config.methods = parse_allow_methods(tokens, i);
-			continue;
-		}
-		if (tokens[i] == "error_page")
-		{
+		else if (tokens[i] == "index")
+			server_config.index = parse_string(tokens, i);
+		else if (tokens[i] == "server_name")
+			server_config.server_name = parse_list(tokens, i);
+		else if (tokens[i] == "allow_methods")
+			server_config.methods = parse_list(tokens, i);
+		else if (tokens[i] == "error_page")
 			server_config.error_page.insert(parse_error_page(tokens, i));
-			continue;
-		}
-		if (tokens[i] == "client_max_body_size")
-		{
+		else if (tokens[i] == "client_max_body_size")
 			server_config.client_max_body_size = parse_client_max_body_size(tokens, i);
-			continue;
-		}
-		if (tokens[i] == "}")
-		{
+		else if (tokens[i] == "location")
+			server_config.location.insert(parse_location(tokens, i));
+		else if (tokens[i] == "}")
 			break ;
-		}
-		i++;
+		else
+			i++;
 	}
 	print_config(server_config);
 	return (server_config);
@@ -243,21 +160,13 @@ std::vector<Config>	parse_config(std::string_view config_path)
 		tokenized_line = tokenize_string(line, "\t\r\n ");
 		if (tokenized_line.empty() || tokenized_line[0] == "#")
 			continue;
-		for (unsigned x = 0; x < tokenized_line.size(); x++)
-		{
-			tokens.push_back(tokenized_line[x]);
-		}
+		tokens.insert(tokens.end(), tokenized_line.begin(), tokenized_line.end());
 	}
-	unsigned long	i;
-	i = 0;
+	unsigned long i = 0;
 	while (i < tokens.size())
 	{
-		if (tokens[i] == "server\0")
-		{
-			Config server_config;
-			server_config = read_config(tokens, i);
-			configs.push_back(server_config);
-		}
+		if (tokens[i] == "server")
+			configs.push_back(read_config(tokens, i));
 		i++;
 	}
 	return (configs);
