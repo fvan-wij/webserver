d4da8dd8d6cac5fa5d7e4a7786cd565925d8f8a3
Duco Ritsema van Eck <ducorits@zonderstreepje.nl>
2025-01-05 14:59:29 +0100

    Added chunked request pytest (But I have no clue if it actually works)

---
+++ pytests/test_chunked_request/conftest.py
@@ -0,0 +1,28 @@
+import pytest
+from os import chdir
+from pathlib import Path
+
+from ..conftest import LogLevel, WebservConfig, search_upwards_for_file
+
+
+@pytest.fixture
+def webserv_config(child_stdout_level: LogLevel) -> WebservConfig:
+    executable_name = 'app'
+    args=["test.conf"]
+    # This should match the ports specified in the config file.
+    ports=[9090]
+    url = "http://localhost:"
+
+    script_path = Path(__file__).parent.resolve()
+    chdir(script_path)
+    executable_path = search_upwards_for_file(executable_name)
+
+
+    config = WebservConfig(
+        path=str(executable_path),
+        args=args,
+        ports=ports,
+        url=url,
+        stdout_level=child_stdout_level,
+        )
+    return config
---
+++ pytests/test_chunked_request/test_chunked_request.py
@@ -0,0 +1,40 @@
+from ..conftest import LogLevel, WebservInstance
+import requests
+import psutil
+
+def send_chunked_post_request(port: int, uri: str, chunks: list, expected_status_code: int):
+	"""
+	Sends a chunked POST request to the specified URI and verifies the response status code.
+
+	Args:
+		port (int): The port number to send the request to.
+		uri (str): The URI to send the request to.
+		chunks (list): The list of chunks to send in the request body.
+		expected_status_code (int): The expected HTTP status code of the response.
+	"""
+	print(f"\nSending chunked POST request to http://localhost:{str(port)}/{uri}")
+
+	def generate_chunks():
+		for chunk in chunks:
+			yield chunk
+
+	headers = {'Transfer-Encoding': 'chunked'}
+	r = requests.post(f"http://localhost:{str(port)}/{uri}", data=generate_chunks(), headers=headers)
+
+	print("Status code == ", r.status_code)
+	print("Expected code == ", expected_status_code)
+	assert r.status_code == expected_status_code
+
+def test_chunked_request(webserv_instance: WebservInstance) -> None:
+	"""
+	Tests the chunked request functionality of the web server instance.
+
+	Args:
+		webserv_instance (WebservInstance): The instance of the web server to test.
+	"""
+	assert webserv_instance.proc.pid != 0
+
+	chunks = ["chunk1", "chunk2", "chunk3"]
+
+	for port in webserv_instance.config.ports:
+		send_chunked_post_request(port, "/upload", chunks, 200)
