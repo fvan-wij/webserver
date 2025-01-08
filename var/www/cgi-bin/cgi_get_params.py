ac69dea0e68de89835bc7ca2c1710a98208c53d1
Joppe Boeve <jboeve@f1r5s18.codam.nl>
2025-01-08 16:34:47 +0100

    Add sleep to python script to test `TIMEOUT`

--- var/www/cgi-bin/cgi_get_params.py
+++ var/www/cgi-bin/cgi_get_params.py
@@ -1,4 +1,5 @@
 import cgi
+import time
 
 print("Listing the parameters given in the URL")
 print("")
@@ -9,3 +10,11 @@ arguments = cgi.FieldStorage()
 for i in arguments.keys():
     print(f"{i} : {arguments[i].value}")
 
+
+time.sleep(5)
+
+# with open("../cgi_test.html") as f:
+#     i: int = 0;
+#     for line in f:
+#         print(f"{i} : {line}")
+#         i += 1
