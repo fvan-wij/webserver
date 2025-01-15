6a4eb934ccdcb895f3e0e2b30e414635ca221f15
Joppe Boeve <jboeve@f1r5s18.codam.nl>
2025-01-15 14:49:53 +0100

    Remove marcros

--- include/meta.hpp
+++ include/meta.hpp
@@ -18,25 +18,6 @@
 
 #define TIME_OUT			8000 // ms
 
-
-#define UNUSED(x) (void)(x)
-#define WARNING(message) \
-	do { \
-		LOG_ERROR("\x1b[0m" << __FILE__ << ":" << __LINE__ << ": WARNING: " << message); \
-	} while (0)
-
-#define UNIMPLEMENTED(message) \
-	do { \
-		LOG_ERROR("\x1b[0m" << __FILE__ << ":" << __LINE__ << ": UNIMPLEMENTED: " << message); \
-		exit(1); \
-	} while (0)
-
-#define UNREACHABLE(message) \
-	do { \
-		LOG_ERROR("\x1b[0m" << __FILE__ << ":" << __LINE__ << ": UNREACHABLE: " << message); \
-		exit(1); \
-	} while (0)
-
 #define RED 	"\x1B[31m"
 #define GREEN 	"\x1B[32m"
 #define YELLOW 	"\x1B[33m"
