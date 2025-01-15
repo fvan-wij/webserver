3ed2a878a06e64d18cc855bcc42454fe16f3c5ae
Joppe Boeve <jboeve@f1r5s18.codam.nl>
2025-01-15 17:04:59 +0100

    Found CGI bug

--- data/server_a/cgi-bin/cgi_get_params.py
+++ data/server_a/cgi-bin/cgi_get_params.py
@@ -1,24 +1,28 @@
-import cgi
-import sys
-import time
-
-print("Listing the parameters given in the URL")
-print("")
-print("")
-print("")
-
-arguments = cgi.FieldStorage()
-for i in arguments.keys():
-    print(f"{i} : {arguments[i].value}")
-
-
-# time.sleep(2)
-
-i = 0
-for arg in sys.argv:
-    print(f"{i} : {arg}")
-    i += 1
-
-# print(sys.argv[3])
-
+# import cgi
+# import sys
+#
+# print("Listing the parameters given in the URL")
+# print("")
+# print("")
+# print("")
+#
+#
+# i = 0
+# for arg in sys.argv:
+#     print(f"{i} : {arg}")
+#     i += 1
+#
+# print('')
+# print('')
+#
+#
+# arguments = cgi.FieldStorage()
+# for i in arguments.keys():
+#     print(f"{i} : {arguments[i].value}")
+#
+#
+#
+# # print(sys.argv[3])
+#
+# print('')
 print('')
